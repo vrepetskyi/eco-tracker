bf1ab36b209b9b2723e9caf7ec6e33fca13317d3
dominikkm <dominik.malkowiak@gmail.com>
2022-12-16 21:18:28 +0100

    graph goes brrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
 import { GlobalStyles } from "@mui/material";
 import Header from "./components/Header";
 
+
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 430, pv: 2410, amt: 600}, {name: 'Page C', uv: 410, pv: 100, amt: 3400}];
+
+
 export default function App() {
+
   return (
     <>
       <GlobalStyles
@@ -13,6 +19,14 @@ export default function App() {
         }}
       />
       <Header />
+      <button onClick={() => setTree(1)}>dshfksdfksdjfh</button>
+      <LineChart width={500} height={300} data={data}>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+      </LineChart>
     </>
   );
 }
