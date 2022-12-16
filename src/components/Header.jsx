1c89c0b4e41b68379b742be2f78d62005126d4ca
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2022-12-16 21:18:17 +0100

    Header WIP

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,59 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
-import { Typography } from "@mui/material";
+import { IconButton, Menu, MenuItem, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Box } from "@mui/system";
+import React from "react";
 
 export default function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const scrollTo = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <AppBar
-      position="static"
+      position="fixed"
       sx={{
         backgroundColor: "#6C804D",
       }}
     >
       <Container maxWidth="sm">
-        <Toolbar sx={{ justifyContent: "center" }} disableGutters>
-          <Typography variant="h6">EcoTracker</Typography>
+        <Toolbar disableGutters>
+          <IconButton size="large" onClick={handleOpenNavMenu} color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorElNav}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+          >
+            <MenuItem onClick={() => scrollTo("tree")}>
+              <Typography>Tree</Typography>
+            </MenuItem>
+            <MenuItem onClick={() => scrollTo("todos")}>
+              <Typography>Todos</Typography>
+            </MenuItem>
+            <MenuItem onClick={() => scrollTo("statistics")}>
+              <Typography>Statistics</Typography>
+            </MenuItem>
+            <MenuItem onClick={() => scrollTo("events")}>
+              <Typography>Events</Typography>
+            </MenuItem>
+          </Menu>
+          <Box sx={{ margin: "auto" }}>
+            <Typography variant="h5">Eco Tracker</Typography>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Josefin Sans', sans-serif",
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
